1b3e2eb7b282f2eec2d8e21268d3b0a485282e61
Shahzaib Naseem <shahzaibnaseem3169@gmail.com>
2026-04-26 18:22:36 +0500

    fix: create db tables on startup

--- app.py
+++ app.py
@@ -68,7 +68,8 @@ def server_error(e):
 
 # ── Init ──────────────────────────────────────────────────────────────────────────
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, port=5000)
